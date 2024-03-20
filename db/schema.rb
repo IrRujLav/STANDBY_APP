3750b43a53007bdb6de0170eb0ce7a6d6792a1ab
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <hashdan542@gmail.com>
2024-03-20 16:12:36 +0100

    Created Venue Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_20_140751) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_20_145338) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,4 +26,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_140751) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "venues", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "location"
+    t.string "name"
+    t.string "type"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_venues_on_user_id"
+  end
+
+  add_foreign_key "venues", "users"
 end
