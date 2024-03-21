c21378073c9ca3418bb4e66d5ca60346a9a5fee4
Irene Rujas Lavandera <pacinha84@gmail.com>
2024-03-21 10:55:33 +0100

    Chaged column name in venues. Don't forget to db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2024_03_20_150518) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_21_095208) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,7 +55,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_150518) do
     t.datetime "updated_at", null: false
     t.string "location"
     t.string "name"
-    t.string "type"
+    t.string "category"
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
