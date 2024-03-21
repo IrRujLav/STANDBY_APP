436267ccf9416c1e6d00f5b8afe3d8a913c87c03
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <hashdan542@gmail.com>
2024-03-21 11:09:25 +0100

    Fixed Venue model validation

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,4 @@
 class Venue < ApplicationRecord
-  validates :location, :name, :type, presence: true
-  validates :type, inclusion: {in: ["Bar/Café", "Theater", "Open air", "Other"]}
+  validates :location, :name, :category, presence: true
+  validates :category, inclusion: {in: ["Bar/Café", "Theater", "Open air", "Other"]}
 end
