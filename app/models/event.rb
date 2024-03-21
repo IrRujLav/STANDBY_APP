aaa1627babbb33f3a7a52aa28c0e7480786bbb63
Jiregna Terfa <jirenlife@gmail.com>
2024-03-21 10:25:39 +0100

    validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :venue
   validates :name, :description, :price, :category, :date, presence: true
+  validates :category, inclusion: {in: ["Stand up comedy", "Improv", "Open mic", "Life music", "Theater"]}
 end
