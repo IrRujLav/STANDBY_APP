3928969a863ebe9fece3cf14f5aa1e0d9bb939c7
Irene Rujas Lavandera <pacinha84@gmail.com>
2024-03-22 18:12:11 +0100

    Home page with the latest events index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "events#home"
   get "up" => "rails/health#show", as: :rails_health_check
   resources :venues, only: [:index]
+
 end
