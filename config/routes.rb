601f9e05abc505ee3ab52d9aebed1ee7addfa1aa
Daniel Hash <108074788+danielhash1@users.noreply.github.com>
2024-03-25 16:01:57 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "events#home"
   get "up" => "rails/health#show", as: :rails_health_check
   resources :venues, only: [:index]
   resources :events, only: [:show]
