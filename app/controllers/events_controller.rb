71c53935e6c993ee07d26a6081277c47945142a7
Jiregna Terfa <jirenlife@gmail.com>
2024-03-26 11:55:42 +0100

    create and update of events completed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,17 +11,32 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
-    @venue = Venue.find(params[:id])
+    @venue = @event.venue
   end
+
+  def new
+    @event = Event.new
+    @venues = Venue.all
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+    redirect_to event_path(@event)
+    else
+    render :new, status: :unprocessable_entity
+  end
+end
 
   def edit
   @event = Event.find(params[:id])
+
   end
 
   def update
   @event = Event.find(params[:id])
   @event.update(event_params)
-  redirect_to edit_event_path(@event)
+  redirect_to events_path(@event)
   end
 
   def destroy
@@ -33,6 +48,6 @@ class EventsController < ApplicationController
 private
 
   def event_params
-    params.require(:event).permit(:name, :description, :date, :price, :category)
+    params.require(:event).permit(:name, :image, :description, :date, :price, :venue_id, :category)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :venue
-  validates :name, :description, :price, :category, :date, presence: true
+  validates :name, :image, :description, :price, :category, :date, presence: true
   validates :category, inclusion: {in: ["Stand up comedy", "Improv", "Open mic", "Live music", "Theater"]}
 end
