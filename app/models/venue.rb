81aef899ba9451e50f3855ed044e7f9366080ec2
Jiregna Terfa <jirenlife@gmail.com>
2024-03-28 12:54:59 +0100

    create, edit, update and delete part of venue has been completed

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,5 @@
 class Venue < ApplicationRecord
+  belongs_to :user
   validates :location, :name, :image, :category, presence: true
   validates :category, inclusion: {in: ["Bar/Café", "Theater", "Open air", "Other"]}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "events#home"
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :venues, only: [:index]
+  resources :venues, only: [:index, :new, :create, :edit, :update, :delete]
   resources :events, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
