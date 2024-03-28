4a15aab69fdc867cb72a62913a0caa4a233f4f89
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <hashdan542@gmail.com>
2024-03-28 11:20:45 +0100

    Venues index done with styling and links

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "events#home"
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :venues, only: [:index]
+  resources :venues, only: [:index, :show]
   resources :events, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
